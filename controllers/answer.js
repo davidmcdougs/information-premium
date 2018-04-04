8e470463766709192b1704bc6982b6927821f6af
akflaherty <anne.flaherty@case.edu>
2018-04-04 19:27:24 -0400

    Updated APIs to handle links between questions and answers

--- controllers/answer.js
+++ controllers/answer.js
@@ -2,32 +2,53 @@
 // ========================
 const db = require("../model");
 module.exports = {
-    // Find all answers
-  findAll: function(req, res) {
+  // Find all answers
+  findAll: function (req, res) {
     db.Answer
       .find({})
-      .then(function(dbAnswer) {
+      .then(function (dbAnswer) {
         res.json(dbAnswer);
       })
       .catch(err => res.status(422).json(err));
   },
   // Find one answer by id
-  findOne: function(req, res) {
+  findOne: function (req, res) {
     db.Answer
       .findOne({
         _id: req.params.id
       })
+      .then(function (dbAnswer) {
+        res.json(dbAnswer);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  // Create a new Answer
+  create: function (req, res) {
+    db.Answer
+      .create(req.body)
+      .then(function (dbAnswer) {
+        res.json(dbAnswer);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  // Update existing answer by id
+  update: function(req, res) {
+    db.Answer
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(function(dbAnswer) {
         res.json(dbAnswer);
       })
       .catch(err => res.status(422).json(err));
   },
-    create: function(req, res) {
-        db.Answer
-            .create(req.body)
-            .then(function(dbAnswer) {
-                res.json(dbAnswer);
-            })
-            .catch(err => res.status(422).json(err));
-    }
+  // Delete an answer by id
+  delete: function (req, res) {
+    db.Answer
+      .remove({
+        _id: req.params.id
+      })
+      .then(function (dbAnswer) {
+        res.json(dbAnswer);
+      })
+      .catch(err => res.status(422).json(err));
+  }
 };
