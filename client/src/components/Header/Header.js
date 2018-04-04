380ce92ea6450aba3c0fdb1d41162ee72168deec
oneillde216 <31212974+oneillde216@users.noreply.github.com>
2018-04-04 14:57:28 -0400

    Add files via upload

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,12 +3,24 @@ import './Header.css';
 import { Card, Icon, Image, Form, TextArea, Button, Header, Segment } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
 
+const colors = {
+	blue: "#6da0f2",
+	white: "#ffffff"
+}
+
+const myStyles = {
+	backgroundColor: colors.blue,
+	Color: colors.white,
+}
+
 const BigHeader = () => (
-  <Segment>
-    <Header as='h3' textAlign='center'>
+ <div style={myStyles}>
+  <Segment color={colors.blue}>
+    <Header as='h3' textAlign='center' color={colors.blue}>
     Information Premium
     </Header>
   </Segment>
+ </div>
 )
 
 export default BigHeader
