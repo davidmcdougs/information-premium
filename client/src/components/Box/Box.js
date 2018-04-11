74931b7c61d39bcca9aa1abea8837c6091fc4afa
oneillde216 <31212974+oneillde216@users.noreply.github.com>
2018-04-11 19:01:29 -0400

    Add files via upload

--- client/src/components/Box/Box.js
+++ client/src/components/Box/Box.js
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Box.css';
-// import { Card, Icon, Image, Form, TextArea, Button } from 'semantic-ui-react';
-import { Card } from 'semantic-ui-react';
+import { Card, Icon, Image, Form, TextArea, Button } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
 
 
 
 const Box = (props) => (
-  <div className="col-sm-6 col-md-4 col-lg-3 fixed-height">
+  <div className="col-sm-6 col-md-4 col-lg-3 fixed-height prettycards">
     <Card className="zoom">
-      <Link to={`/posts/${props.id}`}>
+       <Link to={`/posts/${props.id}`}>
         <Card.Content>
           <Card.Header>
             {props.question}
           </Card.Header>
           <Card.Meta>
           </Card.Meta>
+           <Card.Description className="center">
+          {props.createdBy 
+            ? `Composed By: ${props.createdBy}`
+            : ""
+          }
+        </Card.Description>
           <Card.Description className="center">
-            Question topic: {props.topic}
+            Topic: {props.topic}
           </Card.Description>
-          <Card.Description className="center">
-            Reward: ${props.reward}
+          <Card.Description>
+            {props.reward 
+              ?  "Reward: ${props.reward}"
+              : ""
+            }
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <Card.Description className="center">
-            Number of total responses: {props.totalResponses}
-          </Card.Description>
-          <Card.Description className="center">
-            Question Created by: {props.createdBy}
-          </Card.Description>
+        <Card.Description className="center">
+          {props.totalResponses.length > 0
+            ? `answers: ${props.totalResponses}`
+            : "Be the first to answer this one!"
+          }
+        </Card.Description>
         </Card.Content>
       </Link>
     </Card>
